994a076b7d9432720618d2e24a297ab510eaea0c
Ben Engdahl <bengdahl341@gmail.com>
2021-11-07 16:09:23 -0500

    Update `api`

--- api/syscall.c
+++ api/syscall.c
@@ -5,24 +5,17 @@ uint64_t syscall(uint64_t a, char *target)
   long long r14_out;
   char *r15_out;
 
-  if (target == 0)
-  {
-    static char *null = (char *)0;
-    target = &null;
-  }
-
   asm(
-      "mov %2, %%r14 \n"
-      "mov %3, %%r15 \n"
+      "movq %2, %%r14 \n"
+      "movq %3, %%r15 \n"
       "syscall \n"
-      "mov %%r14, %0 \n"
-      "mov %%r15, %1"
+      "movq %%r14, %0 \n"
+      "movq %%r15, %1"
 
       : "=r"(r14_out),
         "=r"(r15_out)
       : "r"(a),
-        "r"(*target));
+        "r"(target));
 
-  *target = r15_out;
   return r14_out;
 }
