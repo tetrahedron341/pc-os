ac11b52ae690d0456bb706baa5d2e39e897bf1fe
Ben Engdahl <bengdahl341@gmail.com>
2022-01-04 01:51:33 -0500

    Do a bit of decluttering - Begin work on `arch` - Move default panic implementation to a single file - Move test-related functions to one file

---
+++ kernel/src/test.rs
@@ -0,0 +1,61 @@
+use crate::{serial_print, serial_println};
+
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+
+    unreachable!("Should have already exited. Are you not using QEMU?")
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &core::panic::PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
+}
+
+cfg_if::cfg_if! {
+    if #[cfg(test)] {
+        bootloader::entry_point!(test_entry_point);
+
+        /// Entry point for `cargo test`
+        fn test_entry_point(boot_info: &'static mut bootloader::BootInfo) -> ! {
+            crate::init::init(boot_info);
+            crate::test_main();
+            crate::arch::loop_forever();
+        }
+    }
+}
--- kernel/tests/basic_boot.rs
+++ kernel/tests/basic_boot.rs
@@ -1,10 +1,8 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(kernel::test_runner)]
+#![test_runner(kernel::test::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
-use core::panic::PanicInfo;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -13,10 +11,10 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    kernel::test_panic_handler(info);
-}
+// #[panic_handler]
+// fn panic(info: &PanicInfo) -> ! {
+//     kernel::test::test_panic_handler(info);
+// }
 
 #[test_case]
 fn test_println() {
