99a8721fd5b35eba659ad854ad6947e935d65d76
tetrahedron341 <bengdahl341@gmail.com>
2023-05-04 13:47:14 -0400

    Remove unused video code

--- kernel/src/video/mod.rs
+++ kernel/src/video/mod.rs
@@ -1,12 +1,5 @@
-use alloc::boxed::Box;
-use spin::Mutex;
-
 pub use self::framebuffer::Framebuffer;
 
 pub mod console;
 pub mod font;
-// pub mod vesa;
 pub mod framebuffer;
-
-// pub static FRAMEBUFFER: Mutex<Box<dyn Framebuffer + Send>> =
-//     Mutex::new(framebuffer::NullFramebuffer);
