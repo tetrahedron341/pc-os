33eca2050614721d8f35db9f46fb2b90b47eaf89
tetrahedron341 <bengdahl341@gmail.com>
2023-05-04 13:47:14 -0400

    Skeleton for buddy-allocated physical memory

---
+++ kernel/src/arch/x86_64/memory/frame_allocator/mmap.rs
@@ -0,0 +1,40 @@
+use crate::arch::x86_64::memory::{
+    mmap::{MemoryKind, MemoryRegion},
+    PhysAddr,
+};
+use x86_64::structures::paging::{FrameAllocator, PhysFrame, Size4KiB};
+
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static [MemoryRegion],
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(memory_map: &'static [MemoryRegion]) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
+
+    pub fn free_pages(&self) -> usize {
+        self.usable_frames().fold(0, |l, _| l + 1)
+    }
+
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.kind == MemoryKind::Available);
+        let addr_ranges = usable_regions.map(|r| r.start..r.start + r.len);
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+
+        frame_addresses.map(|a| PhysFrame::containing_address(PhysAddr::new(a as u64)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
+}
