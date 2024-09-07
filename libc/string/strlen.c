c09ea4c9b2298f40c136027b86a00ec090a128f3
tetra <19270795+tetrahedron341@users.noreply.github.com>
2024-09-07 15:45:43 -0400

    Create `libc`

---
+++ libc/string/strlen.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+
+size_t strlen(const char *str)
+{
+    size_t len = 0;
+    while (*str != '\0')
+    {
+        str++;
+    }
+    return len;
+}
--- libc/syscall.h
+++ libc/syscall.h
@@ -1,5 +1,3 @@
 #pragma once
 
-#include <stdint.h>
-
 uint64_t syscall(uint64_t a, char *target);
