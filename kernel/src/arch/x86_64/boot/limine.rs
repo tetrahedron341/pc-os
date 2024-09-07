060dbaa8b914f0b5b0b2657f7f413d4ab9f499bb
tetra <19270795+tetrahedron341@users.noreply.github.com>
2024-09-07 15:45:51 -0400

    Load modules and framebuffer in limine

--- kernel/src/arch/x86_64/boot/limine.rs
+++ kernel/src/arch/x86_64/boot/limine.rs
@@ -1,13 +1,23 @@
 use core::mem::MaybeUninit;
 
-use crate::arch::{
-    self,
-    memory::{self, mmap::MemoryRegion, VirtAddr},
+use alloc::{boxed::Box, string::String, vec};
+
+use crate::{
+    arch::{
+        self,
+        memory::{self, mmap::MemoryRegion, VirtAddr},
+    },
+    video::Framebuffer,
 };
 
 static MMAP_REQUEST: limine::LimineMmapRequest = limine::LimineMmapRequest::new(0);
 
 static HHDM_REQUEST: limine::LimineHhdmRequest = limine::LimineHhdmRequest::new(0);
+
+static MODULES_REQUEST: limine::LimineModuleRequest = limine::LimineModuleRequest::new(0);
+
+static FRAMEBUFFER_REQUEST: limine::LimineFramebufferRequest =
+    limine::LimineFramebufferRequest::new(0);
 
 #[no_mangle]
 fn _start() -> ! {
@@ -23,7 +33,6 @@ fn _start() -> ! {
             r.insert(Cr4Flags::OSFXSR | Cr4Flags::OSXMMEXCPT_ENABLE);
         });
     }
-    crate::serial_println!("Hello world!");
     arch::x86_64::interrupts::init_idt();
     arch::x86_64::gdt::init();
 
@@ -52,12 +61,44 @@ fn _start() -> ! {
     crate::allocator::init_heap().unwrap();
     arch::x86_64::syscall::init();
 
-    // let modules = boot_info
-    //     .modules
-    //     .iter()
-    //     .map(|m| unsafe { load_module(*m) })
-    //     .collect();
+    let limine_modules = MODULES_REQUEST
+        .get_response()
+        .get()
+        .and_then(|resp| resp.modules())
+        .unwrap_or_else(|| {
+            crate::serial_println!("Module request failed");
+            &[]
+        });
+    let mut modules = vec![];
+    for m in limine_modules {
+        let name_ptr = m.path.as_ptr().unwrap().cast::<u8>();
+        let mut name_len = 0;
+        while unsafe { name_ptr.offset(name_len).read() } != 0 {
+            name_len += 1;
+        }
+        let name = unsafe {
+            String::from_utf8_lossy(core::slice::from_raw_parts(name_ptr, name_len as usize))
+                .split('/')
+                .last()
+                .unwrap()
+                .into()
+        };
 
+        let data = unsafe {
+            core::slice::from_raw_parts_mut(m.base.as_mut_ptr().unwrap(), m.length as usize)
+        };
+
+        modules.push(crate::boot::BootModule { name, data });
+    }
+
+    let framebuffer = FRAMEBUFFER_REQUEST.get_response().get().and_then(|resp| {
+        resp.framebuffers().map(|fbs| unsafe {
+            let fb = &fbs[0] as *const _;
+            Box::new(FramebufferImpl(
+                &mut *(fb as *mut limine::LimineFramebuffer),
+            )) as Box<dyn Framebuffer + Send + Sync + 'static>
+        })
+    });
     // unsafe {
     //     crate::video::vesa::init_screen(boot_info.framebuffer.as_mut().unwrap());
     // }
@@ -73,8 +114,8 @@ fn _start() -> ! {
     crate::log::init(SERIAL_LOG_MIN, CONSOLE_LOG_MIN, 128);
 
     crate::init::kernel_main(crate::init::InitServices {
-        modules: alloc::vec![],
-        framebuffer: None,
+        modules,
+        framebuffer,
     });
 }
 
@@ -97,6 +138,40 @@ impl From<&limine::LimineMemmapEntry> for memory::mmap::MemoryRegion {
             start: e.base as usize,
             len: e.len as usize,
             kind: e.typ.into(),
+        }
+    }
+}
+
+struct FramebufferImpl(&'static mut limine::LimineFramebuffer);
+
+unsafe impl Send for FramebufferImpl {}
+unsafe impl Sync for FramebufferImpl {}
+
+impl crate::video::Framebuffer for FramebufferImpl {
+    fn info(&self) -> crate::video::framebuffer::FramebufferInfo {
+        crate::video::framebuffer::FramebufferInfo {
+            format: crate::video::framebuffer::PixelFormat {
+                red_shift_bits: self.0.red_mask_shift,
+                red_width_bits: self.0.red_mask_size,
+                green_shift_bits: self.0.green_mask_shift,
+                green_width_bits: self.0.green_mask_size,
+                blue_shift_bits: self.0.blue_mask_shift,
+                blue_width_bits: self.0.blue_mask_size,
+            },
+            bytes_per_pixel: (self.0.bpp as usize) / 8,
+            width: self.0.width as _,
+            height: self.0.height as _,
+            stride: self.0.pitch as _,
+            buffer_len: (self.0.height * self.0.pitch) as _,
+        }
+    }
+
+    fn get_mut(&mut self) -> &mut [u8] {
+        unsafe {
+            core::slice::from_raw_parts_mut(
+                self.0.address.as_mut_ptr().unwrap(),
+                (self.0.height * self.0.pitch) as _,
+            )
         }
     }
 }
