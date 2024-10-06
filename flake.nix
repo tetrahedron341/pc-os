51df695532dac59264132695b96fb60eb4ea4942
tetra <19270795+tetrahedron341@users.noreply.github.com>
2024-10-06 00:20:23 -0400

    Switch to rust-overlay

--- flake.nix
+++ flake.nix
@@ -1,30 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     utils.url = "github:numtide/flake-utils";
     flake-compat.url = "github:edolstra/flake-compat";
   };
 
-  outputs = { self, nixpkgs, utils, nixpkgs-mozilla, flake-compat }:
+  outputs = { self, nixpkgs, utils, rust-overlay, flake-compat }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { overlays = [ nixpkgs-mozilla.overlay ]; inherit system; };
+        pkgs = import nixpkgs { overlays = [ rust-overlay.overlays.default ]; inherit system; };
         limine-override = pkgs.limine.override { enableAll=true; };
-        rustChannel = pkgs.rustChannelOf {
-          rustToolchain = ./rust-toolchain.toml;
-          date = "2024-09-29";
-          sha256 = "sha256-eRCZskam9/DrpAVsoMyvSY7TLnl0E5gfN8FK4gcgZBo=";
-        };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         buildInputs = with pkgs; [ just qemu limine-override jq xorriso OVMF.fd ];
-        nativeBuildInputs = (with rustChannel; [ (rust.override {
-          extensions = [ "rust-src" ];
-        }) ]) 
-          ++ (with pkgs; [ rustfmt clippy ]);
+        nativeBuildInputs = with pkgs; [ rustfmt clippy rustToolchain ];
       in
       {
         devShells.default = with pkgs; mkShell {
-          RUST_SRC_PATH = rustChannel.rust-src;
           OVMF_PATH = "${pkgs.OVMF.fd}/FV/OVMF.fd";
           LIMINE_PREFIX = limine-override;
           inherit buildInputs nativeBuildInputs;
